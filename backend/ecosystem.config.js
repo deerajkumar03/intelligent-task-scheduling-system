ce85de7753f2d28254ae54fa20742519fdd5587b
Deeraj Kumar <deerajkumar79899@gmail.com>
2026-07-21 16:14:12 +0530

    Configure production startup for deployment

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,25 +1,28 @@
 module.exports = {
   apps: [
     {
+      name: "scheduler-server",
+      script: "server.js",
+      instances: 1,
+      exec_mode: "fork",
+    },
+    {
       name: "cpu-worker",
       script: "workers/worker.js",
       instances: 2,
       exec_mode: "fork",
-
       env: {
-  WORKER_TYPE: "cpu"
-}
+        WORKER_TYPE: "cpu",
+      },
     },
-
     {
       name: "io-worker",
       script: "workers/worker.js",
       instances: 2,
       exec_mode: "fork",
-
       env: {
-  WORKER_TYPE: "io"
-}
-    }
-  ]
+        WORKER_TYPE: "io",
+      },
+    },
+  ],
 };
